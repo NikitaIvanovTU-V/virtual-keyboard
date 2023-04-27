e26869af6dd2486c0624a9040d14caa894d4a030
Nikita <7masteryakali@gmail.com>
2023-04-27 09:12:28 +0300

    feat: add shift and caps key functionality

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 import { keyboardNode, keys } from './KeyboardLayout.js';
 
 class Keyboard {
+  constructor() {
+    this.language = 'en';
+    this.caps = false;
+  }
+
   createKeyboard() {
     this.wrapper = document.createElement('main');
     this.wrapper.setAttribute('class', 'wrapper');
@@ -34,28 +39,46 @@ class Keyboard {
     document.addEventListener('keydown', (e) => {
       const key = document.getElementById(e.code);
       if (keys[e.code]) {
-        key.classList.add('active');
-        e.preventDefault();
-        if (!keys[e.code].special) {
-          this.input(key.textContent);
+        if (e.code === 'CapsLock' && !e.repeat) {
+          key.classList.toggle('active');
+          this.caps = !this.caps;
+          this.switchCase(e.shiftKey);
+        } else if ((e.code === 'ShiftLeft' || e.code === 'ShiftRight') && !e.repeat) {
+          this.switchShift(this.language, e.shiftKey);
+          key.classList.add('active');
         } else {
-          switch (e.code) {
-            case 'Backspace':
-              this.backspace();
-              break;
-            case 'Delete':
-              this.delete();
-              break;
-            case 'Tab':
-              this.input('\t');
-              break;
-            case 'Enter':
-              this.input('\n');
-              break;
-            default:
-              break;
+          key.classList.add('active');
+          if (!keys[e.code].special) {
+            e.preventDefault();
+            this.input(key.textContent);
+          } else {
+            switch (e.code) {
+              case 'Backspace':
+                this.backspace();
+                break;
+              case 'Delete':
+                this.delete();
+                break;
+              case 'Tab':
+                this.input('\t');
+                break;
+              case 'Enter':
+                this.input('\n');
+                break;
+              default:
+                break;
+            }
           }
         }
+      }
+    });
+    document.addEventListener('keyup', (e) => {
+      const key = document.getElementById(e.code);
+      if (keys[e.code] && !(e.code === 'CapsLock')) {
+        key.classList.remove('active');
+      } else if ((e.code === 'ShiftLeft' || e.code === 'ShiftRight') && !e.repeat) {
+        this.switchShift(false);
+        key.classList.remove('active');
       }
     });
   }
@@ -73,11 +96,62 @@ class Keyboard {
   delete() {
     const oldSelection = this.textArea.selectionStart;
     this.textArea.value = this.textArea.value.slice(0, this.textArea.selectionStart)
-        + this.textArea.value.slice(this.textArea.selectionEnd + 1);
+      + this.textArea.value.slice(this.textArea.selectionEnd + 1);
     this.textArea.selectionStart = oldSelection;
     this.textArea.selectionEnd = oldSelection;
   }
-}
 
+  switchShift(shift) {
+    if (shift) {
+      Array.from(this.keyboard.querySelectorAll('.keyboard__key')).forEach((e) => {
+        if (!keys[e.id].special) {
+          if (keys[e.id].shift) {
+            e.textContent = keys[e.id].shift[this.language];
+          } else if (this.caps) {
+            e.textContent = e.textContent.toLowerCase();
+          } else {
+            e.textContent = e.textContent.toUpperCase();
+          }
+        }
+      });
+    } else {
+      Array.from(this.keyboard.querySelectorAll('.keyboard__key')).forEach((e) => {
+        if (!keys[e.id].special) {
+          if (keys[e.id].shift) {
+            e.textContent = keys[e.id][this.language];
+          } else if (this.caps) {
+            e.textContent = e.textContent.toUpperCase();
+          } else {
+            e.textContent = e.textContent.toLowerCase();
+          }
+        }
+      });
+    }
+  }
+
+  switchCase(shift) {
+    if ((this.caps && !shift) || (!this.caps && shift)) {
+      this.toUpper();
+    } else {
+      this.toLower();
+    }
+  }
+
+  toLower() {
+    Array.from(this.keyboard.querySelectorAll('.keyboard__key')).forEach((e) => {
+      if (!keys[e.id].special && !keys[e.id].shift) {
+        e.textContent = e.textContent.toLowerCase();
+      }
+    });
+  }
+
+  toUpper() {
+    Array.from(this.keyboard.querySelectorAll('.keyboard__key')).forEach((e) => {
+      if (!keys[e.id].special && !keys[e.id].shift) {
+        e.textContent = e.textContent.toUpperCase();
+      }
+    });
+  }
+}
 const keyboard = new Keyboard();
 keyboard.createKeyboard();
